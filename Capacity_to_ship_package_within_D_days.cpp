7b0359987be28bfff2bd44f9406d6013ba5456c1
Saniya Khan <saniyakhan1709@gmail.com>
2023-02-22 20:01:07 +0530

    Capacity_to_ship_package_within_D_days3.cpp
    Correct code

--- Capacity_to_ship_package_within_D_days.cpp
+++ Capacity_to_ship_package_within_D_days.cpp
@@ -16,36 +16,69 @@
 //             }
 //         }
 
-     class Solution {
+//      class Solution {
+// public:
+//     int shipWithinDays(vector<int>& weights, int days) {
+//         int l, r;
+//         for (int w: weights) {
+//             l = max(l, w);
+//             r += w;
+//         }
+//         int res=r, mid;
+//         while(l<=r){
+//            mid=(r+l)/2;
+//            int ships=1, curcap=mid, n=weights.size();
+//            for(int i=0; i<n;i++){
+//                 if(curcap-weights[i]<0){
+//                     ships=ships+1;
+//                     curcap=mid;
+//                 }
+//                 else
+//                 curcap=curcap-weights[i];
+//            }
+//                 bool valid=ships<=days;
+//             if(valid){
+//                 res=min(res,mid);
+//                 r=mid-1;
+//             }
+//             else {
+//                 l=mid+1;
+//             }
+//             return res;
+//         }
+//       return l;  
+//     }
+// };
+class Solution {
 public:
     int shipWithinDays(vector<int>& weights, int days) {
-        int l, r;
-        for (int w: weights) {
-            l = max(l, w);
-            r += w;
-        }
-        int res=r, mid;
-        while(l<=r){
-           mid=(r+l)/2;
-           int ships=1, curcap=mid, n=weights.size();
-           for(int i=0; i<n;i++){
-                if(curcap-weights[i]<0){
-                    ships=ships+1;
-                    curcap=mid;
-                }
-                else
-                curcap=curcap-weights[i];
-           }
-                bool valid=ships<=days;
-            if(valid){
-                res=min(res,mid);
-                r=mid-1;
-            }
-            else {
-                l=mid+1;
-            }
-            return res;
-        }
-      return l;  
+        int l = 0, r = 0;
+    for (int w: weights) {
+      l = max(l, w);
+      r += w;
     }
+
+    while (l<r) {
+      
+      int mid =(l+ r) / 2;
+      int ship = 1, cur = 0;
+      
+      for (int w : weights) {
+        if (cur+w > mid) {
+           ship=ship+1;
+          cur = 0;
+        }
+        cur += w;
+      }
+
+      if (ship>days) {
+        l =mid+1;
+      } else {
+        r =mid;
+      }
+    }
+
+    return l;
+  } 
+    
 };
